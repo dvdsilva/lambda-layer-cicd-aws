bd7f6f9f90e7a1abe81346e166a90ac6338c2446
dvdsilva <dvdsilva@hotmail.com>
2025-10-03 20:28:14 -0300

    Env Layer

---
+++ nodejs/calculadora.mjs
@@ -0,0 +1,20 @@
+
+/**
+ * Calcula a multiplicaçã de dois numeros
+ * @param {*} x1 
+ * @param {*} x2 
+ * @returns 
+ */
+export function multiplicacao(x1,x2) {
+    return x1 * x2	;
+}
+
+/**
+ * Calcula a multiplicaçã de dois numeros
+ * @param {*} x1 
+ * @param {*} x2 
+ * @returns 
+ */
+export function divisao(x1, x2) {
+    return x1 / x3;
+}
